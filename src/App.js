6de4398359e7f30f3ff1f8c128b74bae00e0a2de
muhila16 <muhila.s2021@vitstudent.ac.in>
2023-11-03 19:36:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import {AiOutlineDelete}  from 'react-icons/ai';
+import {BsCheckLg} from 'react-icons/bs';
 
 function App() {
+  const[isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TO DO LIST</h1>
+      <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+          
+        <div className='todo-input-item'>
+            <label>Title:</label>
+            <input type = "text" placeholder = "Enter Task Title"/>
+        </div>
+
+          <div className='todo-input-item'>
+            <label>Description:</label>
+            <input type = "text" placeholder = "Enter Description"/>
+          </div>
+
+          <div className='todo-input-item'>
+            
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+
+            
+        </div>
+
+        <div className='btn-area'>
+          <button className = {`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>To Do</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
+        </div>
+       
+          <div className='todo-list'>
+    <div className='todo-list-item'>
+      <div>
+        <h3>Study Mern</h3>
+        <p>Watch html,css,Js first.</p>
+      </div>
+      <div>
+        <AiOutlineDelete className='icon' />
+        <BsCheckLg className='check-icon' />
+      </div>
     </div>
+  </div>
+
+      </div>
+    </div>
+    
   );
 }
 
